759c1ed74a7146ca1d333b2b9906f1b3e0bd00bf
KarwaN <karwanusf1@gmail.com>
2025-09-04 02:08:12 +0300

    Create struct-init.swift

---
+++ struct-init.swift
@@ -0,0 +1,17 @@
+struct Employee {
+    var name: String
+    var yearsActive = 0
+}
+
+extension Employee {
+    init() {
+        self.name = "Anonymous"
+        print("Creating an anonymous employee…")
+    }
+}
+
+// creating a named employee now works
+let roslin = Employee(name: "Laura Roslin")
+
+// as does creating an anonymous employee
+let anon = Employee()
